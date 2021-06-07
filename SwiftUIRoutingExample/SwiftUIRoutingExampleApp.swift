b5640844e0e284f5e0a4e45125829d0366637e73
Joe Muers <1000160+joemuers@users.noreply.github.com>
2021-06-07 20:30:58 +0100

    Initial Commit

---
+++ SwiftUIRoutingExample/SwiftUIRoutingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRoutingExampleApp.swift
+//  SwiftUIRoutingExample
+//
+//  Created by joe muers on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRoutingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
